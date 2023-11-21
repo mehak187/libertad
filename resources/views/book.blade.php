1d5c0d63c857ee1cba70f332cb84519e51e1bbc5
mehak-fabtechsol <mehak@fabtechsol.com>
2023-11-21 15:32:45 -0800

    book filter

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -33,9 +33,9 @@
                                 <h3 class="text-white m-0 f-search">Search:</h3>
                             </div>
                             <div class="w-100">
-                                <input type="text"
+                                <input type="text"  id="myinput"
                                     class="form-control sky-bg shadow-none text-dark border border-dark py-2"
-                                    placeholder="Keyword">
+                                    placeholder="Keyword"  onkeyup="searchFunction()">
                             </div>
                             <div class="">
                                 <div class="d-sm-block d-none">
@@ -52,7 +52,7 @@
                     <div class="col-xl-10">
                         <div class="row mt-4" id="content">
                             @foreach ($books as $book)
-                                <div class="col-lg-4 col-sm-6 mt-3">
+                                <div class="col-lg-4 col-sm-6 mt-3 city-result">
                                     <div class="sky-bg rounded-4 p-3 shadow">
                                         <div>
                                             <img src="<?php echo asset('uploads/' . $book['image']); ?>" alt="" class="img-fluid rounded-3">
@@ -60,14 +60,14 @@
                                         <div class="d-flex justify-content-between  mt-3">
                                             <div class="d-flex align-items-center flex-wrap gap-2">
                                                 <h6 class="text-nowrap m-0">Book name:</h6>
-                                                <p class="font-15 m-0">{{ $book['book_name'] }}</p>
+                                                <p class="font-15 m-0 searchable">{{ $book['book_name'] }}</p>
                                             </div>
                                             <a href="{{ $book['link'] }}" class="text-decoration-none text-dark fs-5"><i
                                                     class="fas fa-download"></i></a>
                                         </div>
                                         <div class="d-flex align-items-center gap-2">
                                             <h6 class="m-0">Author:</h6>
-                                            <p class="m-0">{{ $book['author_name'] }}</p>
+                                            <p class="m-0 author">{{ $book['author_name'] }}</p>
                                         </div>
                                         <div>
                                             <h6 class="m-0">Description:</h6>
@@ -104,6 +104,37 @@
     </section>
     @include('template.footer')
     @include('template.jslinks')
+    <script>
+        function searchFunction() {
+            var input, filter, ul, li, a, i, author;
+            input = document.getElementById('myinput');
+            filter = input.value.toUpperCase();
+            li = document.getElementsByClassName('city-result');
+    
+            cusall = document.getElementsByClassName('customer-all-main');
+            var resultsFound = false;
+            for (i = 0; i < li.length; i++) {
+                a = li[i].getElementsByClassName('searchable')[0];
+                author = li[i].getElementsByClassName('author')[0];
+                if (a.innerHTML.toUpperCase().includes(filter) || author.innerHTML.toUpperCase().includes(filter)) {
+                    li[i].style.display = "";
+                    resultsFound = true;
+                } else {
+                    li[i].style.display = 'none';
+                }
+            }
+            document.getElementById('filter-heading').style.display = 'none';
+            for (var j = 0; j < cusall.length; j++) {
+                cusall[j].style.display = 'none';
+            }
+            if (!resultsFound) {
+                document.getElementById('searcherror').style.display = 'block';
+                document.getElementById('error_msg').style.display = 'none';
+            } else {
+                document.getElementById('searcherror').style.display = 'none';
+            }
+        }
+    </script>
 </body>
 
 </html>
